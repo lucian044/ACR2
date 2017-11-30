187bb3875abc9be1ef39505bfb02dbb8294386d6
lmogrovic44 <lbm21@zips.uakron.edu>
2017-11-30 11:05:54 -0500

    get weeks endpoint

---
+++ Controllers/WeekController.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ACR2.Models;
+using ACR2.Models.Resources;
+using ACR2.Persistence;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ACR2.Controllers
+{
+    public class WeekController: Controller
+    {
+        
+        private readonly ACRDbContext context;
+        private readonly IMapper mapper;
+        public WeekController(ACRDbContext context, IMapper mapper)
+        {
+            this.mapper = mapper;
+            this.context = context;
+        }
+
+        [HttpGet("/api/weeks")]
+        public async Task<IEnumerable<WeekResource>> GetWeeks()
+        {
+            var weeks = await context.Week.Include(w => w.Entries).Include(w => w.WeekNumber).ToListAsync();
+
+            return mapper.Map<List<Week>, List<WeekResource>>(weeks);
+        }
+    }
+}
