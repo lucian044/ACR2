497a6e30bd83fe304245267583d5bfc92e0db31a
lmogrovic44 <lbm21@zips.uakron.edu>
2017-12-19 16:46:42 -0500

    update week entry

---
+++ Migrations/20171219214052_AddWeekIdToEntries.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ACR2.Migrations
+{
+    public partial class AddWeekIdToEntries : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_WeekEntry_Week_WeekId",
+                table: "WeekEntry");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "WeekId",
+                table: "WeekEntry",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WeekEntry_Week_WeekId",
+                table: "WeekEntry",
+                column: "WeekId",
+                principalTable: "Week",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_WeekEntry_Week_WeekId",
+                table: "WeekEntry");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "WeekId",
+                table: "WeekEntry",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WeekEntry_Week_WeekId",
+                table: "WeekEntry",
+                column: "WeekId",
+                principalTable: "Week",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
